dd7dab9ddd93e0b1f6529df6a93b4911f48be5d6
chenjl <chenjl@elimen.com.cn>
2021-06-25 20:13:11 +0800

    training framework beta v1

--- src/flower_train.py
+++ src/flower_train.py
@@ -18,32 +18,39 @@ from torchvision.models import resnet18, resnet50
 import torch.nn as nn
 import torch.optim as optim
 from tools.model_trainer import ModelTrainer
+from datetime import datetime
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if __name__ == "__main__":
     
     # step0： 参数配置
+    now_time = datetime.now()
+    time_str = datetime.strftime(now_time,"%m-%d_%H-%M")
+    log_dir = os.path.join(BASE_DIR,"..","results",time_str)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
     train_dir = "/home/elimen/Data/deepshare/Classification/102flowers/train"
     valid_dir = "/home/elimen/Data/deepshare/Classification/102flowers/valid"
-    pretraided_model_path = os.path.join(BASE_DIR, "../pretrained_model","resnet18-5c106cde.pth")
+    pretrained_model_path = "/home/elimen/Data/KylinModelLIB/pretrained_model/resnet18-5c106cde.pth"
 
-    norm_mean = []  # ImageNet 120万图像中统计出来的各通道均值和方差
-    norm_std = []
+    norm_mean = [0.485, 0.456, 0.406]  # ImageNet 120万图像中统计出来的各通道均值和方差
+    norm_std = [0.229, 0.224, 0.225]
     normTransform = transforms.Normalize(norm_mean, norm_std)
 
     transforms_train = transforms.Compose([
-        transforms.Resize(32),
+        transforms.Resize((256)),  # (256, 256) 区别； （256） 最短边256
+        transforms.CenterCrop(256),
+        transforms.RandomCrop(224),
         transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomCrop(32, padding=4),
         transforms.ToTensor(),
-        normTransform
+        normTransform,
     ])
-
     transforms_valid = transforms.Compose([
-        transforms.Resize((32, 32)),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        normTransform
+        normTransform,
     ])
 
     train_bs = 128
@@ -56,22 +63,23 @@ if __name__ == "__main__":
     factor = 0.1
     milestones = [30,45]
     max_epoch = 50
+    log_interval = 10
     
 
     # step1： 数据集
     # 构建 MyDataset 实例
-    train_data = FlowerDataset(root_dir=train_dir,transform=transforms_train)
-    valid_data = FlowerDataset(root_dir=valid_dir,transform=transforms_valid)
+    train_data = FlowerDataset(root_dir=train_dir, transform=transforms_train)
+    valid_data = FlowerDataset(root_dir=valid_dir, transform=transforms_valid)
     # 构建 DataLoader
-    train_loader = DataLoader(dataset=train_data,batch_size=train_bs,shuffle=True,num_workers=workers)
-    valid_loader = DataLoader(dataset=valid_data,batch_size=valid_bs,shuffle=False,num_workers=workers)
+    train_loader = DataLoader(dataset=train_data, batch_size=train_bs, shuffle=True, num_workers=workers)
+    valid_loader = DataLoader(dataset=valid_data, batch_size=valid_bs, shuffle=False, num_workers=workers)
     
     # step2： 模型
-    model = resnet18(pretrained=True)
+    model = resnet18()
 
     # 导入预训练模型
-    if os.path.exists(pretraided_model_path):
-        pretrained_state_dict = torch.load(pretraided_model_path, map_location='cpu')
+    if os.path.exists(pretrained_model_path):
+        pretrained_state_dict = torch.load(pretrained_model_path, map_location='cpu')
         model.load_state_dict(pretrained_state_dict)
     # 修改输出层
     num_ftrs = model.fc.in_features
@@ -80,9 +88,35 @@ if __name__ == "__main__":
 
     # step3： 损失函数、优化器
     loss_f = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(),lr=lr_init,momentum=momentum,weight_decay=weight_decay)
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer,gamma=factor,milestones=milestones)
+    optimizer = optim.SGD(model.parameters(), lr=lr_init, momentum=momentum, weight_decay=weight_decay)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, gamma=factor, milestones=milestones)
 
     # step4： 迭代训练
+    loss_rec = {"train":[],"valid":[]}
+    acc_rec = {"train":[],"valid":[]}
+    best_acc, best_epoch = 0, 0
+
     for epoch in range(max_epoch):
-        for 
+        loss_train, acc_train, mat_train, path_error_train = ModelTrainer.train(
+                train_loader, model, loss_f, optimizer, scheduler, epoch, device, log_interval, max_epoch)
+        
+        loss_valid, acc_valid, mat_valid, path_error_valid = ModelTrainer.valid(
+            valid_loader, model, loss_f, device)
+        
+        print("Summary: Epoch[{:0>3}/{:0>3}] || Train Acc: {:.2%} , Valid Acc:{:.2%} || Train loss:{:.4f} , Valid loss:{:.4f} || LR:{}". \
+                    format(epoch + 1, max_epoch, acc_train, acc_valid, loss_train, loss_valid,
+                           optimizer.param_groups[0]["lr"]))
+
+        scheduler.step()
+
+        # 模型保存
+        if best_acc < acc_valid or epoch == max_epoch - 1:
+            best_epoch = epoch if best_acc < acc_valid else best_epoch
+            best_acc = acc_valid if best_acc < acc_valid else best_acc            
+            checkpoint = {"model_state_dict": model.state_dict(),
+                          "optimizer_state_dict": optimizer.state_dict(),
+                          "epoch":epoch,
+                          "best_acc":best_acc}
+            pkl_name = "checkpoint_{}.pkl".format(epoch) if epoch == max_epoch -1 else "checkpoint_best.pkl"
+            path_checkpoint = os.path.join(log_dir, pkl_name)
+            torch.save(checkpoint, path_checkpoint)
--- tools/model_trainer.py
+++ tools/model_trainer.py
@@ -8,28 +8,28 @@
 
 import torch
 import numpy as np
-from collection import Counter
+from collections import Counter
 
 class ModelTrainer(object):
     
     @staticmethod
-    def train(data_loader,model,loss_f,optimizer,scheduler,epoch_idx,device,log_interval,max_epoch):
+    def train(data_loader, model, loss_f, optimizer, scheduler, epoch_idx, device, log_interval, max_epoch):
         model.train()
 
         class_num = data_loader.dataset.cls_num
-        conf_mat = np.zeros((class_num,class_num)))
+        conf_mat = np.zeros((class_num, class_num))
         loss_sigma = []
         loss_mean = 0
         acc_avg = 0
         path_error = []
         label_list = []
 
-        for i, data in enumerate(dataloader):
+        for i, data in enumerate(data_loader):
             
-            _, label, _ = data
-            label_list.expend(label.tolist())
+            _, label = data
+            label_list.extend(label.tolist())
 
-            inputs, labels, path_imgs = data
+            inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
             
             # forward & backward
@@ -42,11 +42,50 @@ class ModelTrainer(object):
             # loss statistics
             loss_sigma.append(loss.item())
             loss_mean = np.mean(loss_sigma)
+
+            _, predicted = torch.max(outputs.data,1)
+            for j in range(len(labels)):
+                cate_i = labels[j].cpu().numpy()
+                pre_i = predicted[j].cpu().numpy()
+                conf_mat[cate_i, pre_i] += 1
             acc_avg = conf_mat.trace() / conf_mat.sum()
 
             # Print every 10 iterations
             if i%log_interval == log_interval -1:
-                print("Training: Epoch[{}/{}] Iteration[{}/{}] Loss:{} Acc:{}".format(epoch_idx+1,max_epoch,i+1,len(data_loader),loss_mean,acc_avg))
+                print("Training: Epoch[{}/{}] || Iteration[{}/{}] || Loss:{} || Acc:{}".format(epoch_idx+1,max_epoch,i+1,len(data_loader),loss_mean,acc_avg))
         
-        print("epoch:{} sampler:{}".format(epoch_idx, Counter(label_list)))
+        # print("Epoch:{} sampler:{}".format(epoch_idx, Counter(label_list)))
         return loss_mean, acc_avg, conf_mat, path_error
+
+
+    @staticmethod
+    def valid(data_loader, model, loss_f, device):
+        model.eval()
+
+        class_num = data_loader.dataset.cls_num
+        conf_mat = np.zeros((class_num, class_num))
+        loss_sigma = []
+        path_error = []
+
+        for i, data in enumerate(data_loader):
+            # inputs, labels, path_imgs = data
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs)
+            loss = loss_f(outputs.cpu(), labels.cpu())
+
+            # 统计混淆矩阵
+            _, predicted = torch.max(outputs.data, 1)
+            for j in range(len(labels)):
+                cate_i = labels[j].cpu().numpy()
+                pre_i = predicted[j].cpu().numpy()
+                conf_mat[cate_i, pre_i] += 1.
+
+            # 统计loss
+            loss_sigma.append(loss.item())
+
+        acc_avg = conf_mat.trace() / conf_mat.sum()
+
+        return np.mean(loss_sigma), acc_avg, conf_mat, path_error
+
