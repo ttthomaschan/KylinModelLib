d5f3dc6554c40adef8c516f58125f7ac3d748be3
chenjl <chenjl@elimen.com.cn>
2021-09-08 21:39:24 +0800

    modify common_tools v2

--- tools/common_tools.py
+++ tools/common_tools.py
@@ -35,75 +35,53 @@ def check_dir(path_tmp):
     assert os.path.exists(path_tmp), \
         "\n\n路径不存在，当前变量中指定的路径是：\n{}\n请检查相对路径的设置，或者文件是否存在".format(os.path.abspath(path_tmp))
 
-def show_confMat(confusion_mat, classes, set_name, out_dir, epoch=999, verbose=False, figsize=None, perc=False):
+class Logger(object):
+    def __init__(self, log_path):
+        log_name = os.path.basename(log_path)  ## 截取文件名
+        log_dir = os.path.dirname(log_path)    ## 截取路径
+        self.log_name = log_name if log_name else "root"
+        self.out_path = log_path
+
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+
+    def init_logger(self):
+        logger = logging.getLogger(self.log_name)
+        logger.setLevel(level=logging.INFO)
+
+        # 配置文件Handler
+        file_handler = logging.FileHandler(self.out_path, 'w')
+        file_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+
+        # 配置屏幕Handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+
+        # 添加Handler
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+        return logger
+
+def make_logger(out_dir):
     '''
-    混淆矩阵绘制 并 保存图片
-    :param confusion_mat:  ndarray
-    :param classes: list or tuple, 类别名称
-    :param set_name: str， 数据集名称 train or valid or test
-    :param out_dir: str， 保存地址
-    :param epoch: int， 第几个 epoch
-    :param verbose: bool， 是否打印精度信息
-    :param perc: bool, 是否采用百分比 （ 图像分割时使用，分类数量过大）
+    在out_dir文件夹下以当前时间命名，创建日志文件夹，并创建logger实例，用于记录信息
+    :param out_dir:  str
     :return:
     '''
+    now_time = datetime.now()
+    time_str = datetime.strftime(now_time,"%m-%d_%H-%M")
+    log_dir = os.path.join(out_dir, time_str)
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+    # 创建logger
+    log_path = os.path.join(log_dir, "log.log")
+    logger = Logger(log_path)
+    logger = logger.init_logger()
+    return logger, log_dir
 
-    cls_num = len(classes)
-
-    # 归一化
-    confusion_mat_tmp = confusion_mat.copy()
-    for i in range(len(classes)):
-        confusion_mat_tmp[i, :] = confusion_mat[i, :] / confusion_mat[i, :].sum()
-
-    # 设置图像大小
-    if cls_num < 10:
-        figsize = 6
-    elif cls_num >= 100:
-        figsize = 30
-    else:
-        figsize = np.linspace(6, 30, 91)[cls_num - 10]
-    plt.figure(figsize=(int(figsize),int(figsize*1.3)))
-
-    # 获取颜色
-    cmap = plt.cm.get_cmap('Greys')   # 更多颜色: http://matplotlib.org/examples/color/colormaps_reference.html
-    plt.imshow(confusion_mat_tmp, cmap=cmap)
-    plt.colorbar(fraction=0.03)
-
-    # 设置文字
-    xlocations = np.array(range(len(classes)))
-    plt.xticks(xlocations, list(classes), rotation=60)
-    plt.yticks(xlocations, list(classes))
-    plt.xlabel('Predict label')
-    plt.ylabel('True label')
-    plt.title("Confusion_Matrix_{}_{}".format(set_name, epoch))
-
-    # 打印数字
-    if perc:
-        cls_per_nums = confusion_mat.sum(axis=0)
-        conf_mat_per = confusion_mat / cls_per_nums
-        for i in range(confusion_mat_tmp.shape[0]):
-            for j in range(confusion_mat_tmp.shape[1]):
-                plt.text(x=j,y=i,s=":.0%".format(conf_mat_per[i,j]),
-                         va='center', ha='center', color='red',
-                         fontsize=10)
-    else:
-        for i in range(confusion_mat_tmp.shape[0]):
-            for j in range(confusion_mat_tmp.shape[1]):
-                plt.text(x=j, y=i, s=int(confusion_mat[i, j]),
-                         va='center', ha='center', color='red',
-                         fontsize=10)
-
-    # 保存
-    plt.savefig(os.path.join(out_dir,"Confusion_Matrix_{}.png".format(set_name)))
-    plt.close()
-
-    if verbose:
-        for i in range(cls_num):
-            print('class:{:<10}, total num:{:<6}, correct num:{:<5} Recall:{:.2%} Precision:{:.2%}'.format(
-                classes[i], np.sum(confusion_mat[i, :]), confusion_mat[i,i],
-                confusion_mat[i,i] / (1e-9 + np.sum(confusion_mat[i, :])),
-                confusion_mat[i, i] / (1e-9 + np.sum(confusion_mat[:, i]))
-            ))
 
 def plot_line(train_x, train_y, valid_x, valid_y, mode, out_dir):
     '''
@@ -127,91 +105,75 @@ def plot_line(train_x, train_y, valid_x, valid_y, mode, out_dir):
     plt.savefig(os.path.join(out_dir, mode + '.png'))
     plt.close()
 
-class Logger(object):
-    def __init__(self, path_log):
-        log_name = os.path.basename(path_log)
-        self.log_name = log_name if log_name else "root"
-        self.out_path = path_log
 
-        log_dir = os.path.dirname(self.out_path)
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
 
-    def init_logger(self):
-        logger = logging.getLogger(self.log_name)
-        logger.setLevel(level=logging.INFO)
-
-        # 配置 文件Handler
-        file_handler = logging.FileHandler(self.out_path, "w")
-        file_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
-
-        # 配置 屏幕Handler
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-
-        # 添加handler
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
-
-        return logger
-
-def make_logger(out_dir):
+def show_confMat(confusion_mat, classes, set_name, out_dir, epoch=999, verbose=False, percentage=False):
     '''
-    在 out_dir 文件夹下以当前时间命名，创建日志文件夹，并创建logger用于记录信息
-    :param out_dir:
-    :return:
+    混淆矩阵绘制并保存图片
+    :param confusion_mat:  nd.array
+    :param classes:  list or tuple, 类别名称
+    :param set_name:  str, 数据集类型 train/valid/test
+    :param out_dir:  str, 图片保存文件夹
+    :param epoch:  int, 第几个epoch
+    :param verbose:  bool， 是否打印精度信息
+    :param percentage: bool, 是否采用百分比【一般图像分割时使用，因各分类数目大】
+    :return: 
     '''
-    now_time = datetime.now()
-    time_str = datetime.strftime(now_time, '%m-%d_%H-%M')
-    log_dir = os.path.join(out_dir, time_str)
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    # 创建logger
-    path_log = os.path.join(log_dir, 'log.log')
-    logger = Logger(path_log)
-    logger = logger.init_logger()
-    return logger, log_dir
 
-def get_model(cfg, cls_num, logger):
-    '''
-    创建模型
-    :param cfg:
-    :param cls_num:
-    :param logger:
-    :return:
-    '''
-    if cfg.model_name == "resnet18":
-        model = resnet18()
-        if os.path.exists(cfg.path_resnet18):
-            pretrained_state_dict = torch.load(cfg.path_resnet18, map_location="cpu")
-            model.load_state_dict(pretrained_state_dict) # load pretrain model
-            logger.info("Load pretrained model.")
-        # 修改最后一层
-        num_ftrs = model.fc.in_features
-        model.fc = nn.Linear(num_ftrs, cls_num)  # 102
+    cls_num = len(classes)
+    confusion_mat_tmp = confusion_mat.copy()
 
-    elif cfg.model_name == "vgg16_bn":
-        model = vgg16_bn()
-        if os.path.exists(cfg.path_vgg16bn):
-            pretrained_state_dict = torch.load(cfg.path_vgg16bn, map_location="cpu")
-            model.load_state_dict(pretrained_state_dict)
-            logger.info("Load pretrained model.")
-        # 替换网络层
-        in_feat_num = model.classifier[6].in_features
-        model.classfier[6] = nn.Linear(in_feat_num, cls_num)
+    # 逐行归一化
+    for i in range(cls_num):
+        confusion_mat_tmp[i,:] = confusion_mat[i,:]/confusion_mat[i,:].sum()
 
-    elif cfg.model_name == "se_resnet50":
-        model = se_resnet50()
-        if os.path.exists(cfg.path_se_res50):
-            model.load_state_dict(torch.load(cfg.path_se_res50))
-            logger.info("Load pretrained model.")
-        in_feat_num = model.fc.in_features
-        model.fc = nn.Linear(in_feat_num, cls_num)
-
+    # 设置图像大小
+    if cls_num < 10:
+        figsize = 6
+    elif cls_num >= 100:
+        figsize = 30
     else:
-        raise Exception("Invalid model name. got {}".format(cfg.model_name))
+        figsize = np.linspace(6,30,91)[cls_num-10]
+    plt.figure(figsize=(int(figsize),int(figsize*1.3)))
 
-    return model
+    # 获取颜色
+    cmap = plt.cm.get_cmap('Greys')  # 更多颜色： http://matplotlib.org/examples/color/colormaps_reference.html
+    plt.imshow(confusion_mat_tmp, cmap=cmap)
+    plt.colorbar(fraction=0.03)
+
+    # 设置文字
+    xlocation = np.array(range(cls_num))
+    plt.xticks(xlocation, list(classes), rotation=60)
+    plt.yticks(xlocation, list(classes))
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.title('Confusion_Matrix_{}_{}'.format(set_name, epoch))
+
+    # 打印数字
+    if percentage:
+        cls_per_nums = confusion_mat.sum(axis=1).reshape((cls_num, 1))
+        conf_mat_perc = confusion_mat / cls_per_nums
+        for i in range(confusion_mat_tmp.shape[0]):
+            for j in range(confusion_mat_tmp.shape[1]):
+                plt.text(x=j, y=i, s="{:.0%}".format(conf_mat_perc[i,j]),
+                         va='center', ha='center',
+                         color='red', fontsize=10)
+    else:
+        for i in range(confusion_mat_tmp.shape[0]):
+            for j in range(confusion_mat_tmp.shape[1]):
+                plt.text(x=j, y=i, s=int(confusion_mat[i,j]),
+                         va='center', ha='center',
+                         color='red', fontsize=10)
+
+    # 保存
+    plt.savefig(os.path.join(out_dir, "Confusion_Matrix_{}.png".format(set_name)))
+    plt.close()
+
+    if verbose:
+        for i in range(cls_num):
+            print('class:{:<10}, total_num:{:<6}, correct_num:{:<5} | Recall: {:.2%} Precision: {:.2%}'.format(
+                classes[i], np.sum(confusion_mat[i,:]), confusion_mat[i,i],
+                confusion_mat[i,i] / (.1 + np.sum(confusion_mat[i, :])),
+                confusion_mat[i,i] / (.1 + np.sum(confusion_mat[:, i]))))
+
 
